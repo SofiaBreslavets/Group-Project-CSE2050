52fa22a513c449c1fb1758edbf54a9a459c19239
camin2007 <camin080807@gmail.com>
2026-09-20 23:10:33 -0400

    Update customer.py

--- customer.py
+++ customer.py
@@ -4,7 +4,7 @@ class Customer():
         """ Initializes customer class with an id, name, and a shopping cart"""
         self.customer_id = customer_id
         self.name = name
-        self.cart = shopping_cart.ShoppingCart()
+        self.cart = ShoppingCart()
         return
 
     def get_id(self):
