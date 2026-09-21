3607a2fa684a71a79fc31a27142ded77acdcd566
camin2007 <camin080807@gmail.com>
2026-09-20 21:44:54 -0400

    delete

--- store.py
+++ store.py
@@ -1,11 +1,1 @@
-class store():
-    def __init__(self):
-        """initializes the store"""
-        self.customer = []
-        self.product = []
 
-    def add_customer(self, customer):
-        """Adds customer to database"""
-        self.customer.append(customer)
-
-    def 
