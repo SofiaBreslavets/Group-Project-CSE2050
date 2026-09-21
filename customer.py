bae233134333013bce31c9b0ec7f2dfe0ef9572e
camin2007 <camin080807@gmail.com>
2026-09-20 23:23:37 -0400

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,22 +1,23 @@
-import cart
-class Customer():
+from cart import ShoppingCart
+
+
+class Customer:
     def __init__(self, customer_id: str, name: str):
-        """ Initializes customer class with an id, name, and a shopping cart"""
+        """Initializes customer class with an id, name, and a shopping cart"""
         self.customer_id = customer_id
         self.name = name
         self.cart = ShoppingCart()
-        return
 
     def get_id(self):
-        """ Returns customer's id"""
+        """Returns customer's id"""
         return self.customer_id
 
     def get_name(self):
-        """ Returns customer's name"""
+        """Returns customer's name"""
         return self.name
 
     def get_cart(self):
-        """ Returns customer's shopping cart"""
+        """Returns customer's shopping cart"""
         return self.cart
 
     def __repr__(self):
